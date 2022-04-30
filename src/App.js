a6dd7c8ca881665e63e05432b72ec0a31eeb39e1
Kauan Vaz <kauanvazc@gmail.com>
2022-04-30 16:43:32 -0300

    titulo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Aula GitHub Actions
+          EIS A QUESTÃO
         </p>
         <a
           className="App-link"
